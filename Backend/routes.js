60a639f0564a29c312631be97ba44f9b386012c4
otavigoto <otavio2912@gmail.com>
2026-04-22 22:52:35 -0300

    Setup inicial das rotas

---
+++ Backend/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+
+//Adicionar as rotas aqui
+
+/*
+router.get('/', (req, res) => {
+  res.send('Bem-vindo à API');
+});
+*/
+
+module.exports = router;
