357ed91c597b35e6cdf3eee3f1737e0311a82cbe
Bruno Cesar <brunocesarglm@gmail.com>
2026-05-22 19:42:24 -0300

    erro de export corrigido

--- Backend/functions/src/users/index.ts
+++ Backend/functions/src/users/index.ts
@@ -3,5 +3,4 @@
 */
 export {createUser} from "./handlers/createUser";
 export { loginUser } from "./handlers/loginUser";
-export {checkEmail} from "./handlers/checkEmail";
 export {forgotPassword} from "./handlers/forgotPassword";
